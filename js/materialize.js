43c6ed11b91d9d206910296beab79f0dc51c2b95
jeissler <jeissler0@gmail.com>
2016-02-29 12:23:34 -0600

    sass and js compile materialize framework core for code kit

---
+++ js/materialize.js
@@ -0,0 +1,37 @@
+// @codekit-append "../bower_components/Materialize/js/initial.js";
+// @codekit-append "../bower_components/Materialize/js/jquery.easing.1.3.js";
+// @codekit-append "../bower_components/Materialize/js/animation.js";
+// @codekit-append "../bower_components/Materialize/js/velocity.min.js";
+// @codekit-append "../bower_components/Materialize/js/hammer.min.js";
+// @codekit-append "../bower_components/Materialize/js/jquery.hammer.js";
+// @codekit-append "../bower_components/Materialize/js/global.js";
+
+//  "../bower_components/Materialize/js/collapsible.js";
+//  "../bower_components/Materialize/js/dropdown.js";
+//  "../bower_components/Materialize/js/leanModal.js";
+
+// @codekit-append "src/custom_materialbox.js";
+// @codekit-append "src/custom_parallax.js";
+
+//  "../bower_components/Materialize/js/tabs.js";
+//  "../bower_components/Materialize/js/tooltip.js";
+//  "../bower_components/Materialize/js/waves.js";
+//  "../bower_components/Materialize/js/toasts.js";
+//  "../bower_components/Materialize/js/sideNav.js";
+
+// @codekit-append "src/custom_scrollspy.js";
+
+//  "../bower_components/Materialize/js/forms.js";
+//  "../bower_components/Materialize/js/slider.js";
+//  "../bower_components/Materialize/js/cards.js";
+
+// @codekit-append "../bower_components/Materialize/js/chips.js";
+
+//  "../bower_components/Materialize/js/pushpin.js";
+//  "../bower_components/Materialize/js/buttons.js";
+//  "../bower_components/Materialize/js/transitions.js";
+//  "../bower_components/Materialize/js/scrollFire.js";
+//  "../bower_components/Materialize/js/date_picker/picker.js";
+//  "../bower_components/Materialize/js/date_picker/picker.date.js";
+//  "../bower_components/Materialize/js/character_counter.js";
+//  "../bower_components/Materialize/js/carousel.js";
